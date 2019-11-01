066fb79a431df982272fcfd7e6dc74ae71cb553f
Mustafa Dalğa <mustafadalgaa@gmail.com>
2019-11-01 20:38:13 +0300

    Proje Tamamlandı

---
+++ YasadigimGunSayisi/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace YasadigimGunSayisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.dtarih = new System.Windows.Forms.DateTimePicker();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.ForeColor = System.Drawing.Color.White;
+            this.lblSonuc.Location = new System.Drawing.Point(100, 180);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(300, 23);
+            this.lblSonuc.TabIndex = 10;
+            this.lblSonuc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl1
+            // 
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl1.ForeColor = System.Drawing.Color.White;
+            this.lbl1.Location = new System.Drawing.Point(75, 30);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(350, 26);
+            this.lbl1.TabIndex = 9;
+            this.lbl1.Text = "Doğum Gününüzü Giriniz";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dtarih
+            // 
+            this.dtarih.AllowDrop = true;
+            this.dtarih.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dtarih.Location = new System.Drawing.Point(75, 58);
+            this.dtarih.Name = "dtarih";
+            this.dtarih.Size = new System.Drawing.Size(350, 26);
+            this.dtarih.TabIndex = 8;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.Black;
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.ForeColor = System.Drawing.Color.White;
+            this.btnHesapla.Location = new System.Drawing.Point(75, 90);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(350, 36);
+            this.btnHesapla.TabIndex = 11;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.dtarih);
+            this.Controls.Add(this.lblSonuc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kaç Gündür Yaşıyorsun ?";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.DateTimePicker dtarih;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ YasadigimGunSayisi/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace YasadigimGunSayisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int gun, ay, yil;
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            DateTime tarih = dtarih.Value;
+            gun = tarih.Day;
+            ay = tarih.Month;
+            yil = tarih.Year;
+            DateTime dogumTarihi = new DateTime(yil, ay, gun);
+            DateTime simdi = DateTime.Now;
+
+            if(dogumTarihi>simdi)
+            {
+                lblSonuc.Text = "";
+                MessageBox.Show("Lütfen geçerli bir doğum tarihi giriniz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                TimeSpan gunfarkı = (simdi - dogumTarihi);
+                int sonuc = gunfarkı.Days;
+                lblSonuc.Text = "Toplam " + sonuc + " gündür yaşıyorsunuz.";
+
+            }
+
+      
+        }
+    }
+}
